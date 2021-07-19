ed2d423ab8fdb948df5d9316b46bea209ca16ef7
theCarlson <rjcarlson24@gmail.com>
2021-07-19 14:52:51 -0500

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,6 +128,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function() {
+  console.log('Welcome to the Application!');
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -136,6 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
